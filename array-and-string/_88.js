d5d05eb8f3089477d84f751f34b369655d3affb5
Nguyen Thanh Son [sown] <nguyenthanhson162001@gmail.com>
2024-03-30 21:16:22 +0700

    88

--- array-and-string/_88.js
+++ array-and-string/_88.js
@@ -1,20 +1,31 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
 var merge = function (nums1, m, nums2, n) {
 
     for (let i2 = 0; i2 < n; i2++) {
+        const indexInsert = 0;
         for (let i1 = 0; i1 < m; i1++) {
-            if (nums1[i1] > nums2[i2]) {
-                for (let k = m + i2; k > i1; k--) {
-                    nums1[k] == nums1[k - 1]
-                }
-                nums1[i1] = nums2[i2]
-                break;
-            }
-        }
-    }
-}
 
-// Test cases
-let nums1, m, nums2, n;
-// 1
-nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
-console.log(merge(nums1, m, nums2, n)) // [1,2,2,3,5,6]
+        }
+        insertElement(nums1, indexInsert, nums2[i2])
+
+    }
+
+};
+
+var insertElement = function (nums1, index, value) {
+    for (let i1 = 0; i1 < m; i1++) {
+
+        for (let k = m + i2; k > i1; k--) {
+            nums1[k] == nums1[k - 1]
+        }
+        nums1[i1] = nums2[i2]
+        break;
+    }
+
+}
