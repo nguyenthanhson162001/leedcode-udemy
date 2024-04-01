a516ef7f769e8b487e8a8fb75f06111434854313
Nguyen Thanh Son [sown] <nguyenthanhson162001@gmail.com>
2024-04-01 19:01:48 +0700

    finish 88

--- array-and-string/_88.js
+++ array-and-string/_88.js
@@ -5,23 +5,24 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {
+const merge = function (nums1, m, nums2, n) {
     for (let i2 = 0; i2 < n; i2++) {
-        let indexInsert = 0;
-        for (let i1 = 0; i1 < m; i1++) {
-            if (nums2[i2] <= nums1[i1])
+        let indexInsert = m + i2;
+        for (let i1 = 0; i1 < m + i2; i1++) {
+            if (nums2[i2] <= nums1[i1]) {
                 indexInsert = i1
+                break
+            }
+
         }
         insertElement(nums1, m + i2, indexInsert, nums2[i2])
-
     }
-
 };
 
-var insertElement = function (nums1, length, index, value) {
-    for (let i = length; i >= 0; i--) {
+const insertElement = function (nums1, length, index, value) {
+    for (let i = length; i > index; i--) {
         nums1[i] = nums1[i - 1]
     }
-
+    nums1[index] = value
 }
 
